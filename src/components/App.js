3222b40020688621dd48fb30e9a0be7dbf0ce9ec
Veysel Usta <37637870+veyselusta@users.noreply.github.com>
2020-11-04 13:40:37 +0300

    project completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "../styles.css";
 
 export default function App() {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [total, setTotal] = useState(number1 + number2);
+
+  const calculate = () => {
+    setTotal(number1 + number2);
+  };
+
+  const changeFirst = (event) => {
+    let numFirst = +event.target.value;
+    setNumber1(numFirst);
+  };
+
+  const changeSecond = (event) => {
+    let numSecond = +event.target.value;
+    setNumber2(numSecond);
+  };
+
   return (
     <div className="App">
       <h1>Adding Two Numbers</h1>
 
       <div className="number-inputs">
-        <input type="number" placeholder="0" />
-        <input type="number" placeholder="0" />
+        <input onChange={changeFirst} type="number" placeholder="0" />
+        <input onChange={changeSecond} type="number" placeholder="0" />
       </div>
 
-      <button>Add Them!</button>
+      <button onClick={calculate}>Add Them!</button>
 
-      <h2>0</h2>
+      <h2>{total}</h2>
     </div>
   );
 }
